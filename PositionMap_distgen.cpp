93ac0a528a8ed63965413989ff0519f6b9b7d5f8
“benni-corcione” <benedetta.corcione@uniroma1.it>
2025-02-23 22:22:21 +0100

    latest version

--- PositionMap_distgen.cpp
+++ PositionMap_distgen.cpp
@@ -26,8 +26,9 @@ Double_t Cruijff(Double_t *x, Double_t *par);
 Double_t Landau(Double_t *x, Double_t *par);
 Double_t AG(Double_t *x, Double_t *par);
 Double_t CrystalBall(Double_t *x, Double_t *par);
-void DrawPad(TPad* pad1,TH2F* map,int range, int tes_side, int cnt_radius);
+void DrawPad(TH2F* map,int range, int tes_side, int cnt_radius);
 int GetDepartureRange(int cnt_radius);
+int GetArrivalRangeZoom(int cnt_radius);
 int GetArrivalRange(int cnt_radius);
 int main(int argc, char* argv[]){
   
@@ -50,7 +51,7 @@ int main(int argc, char* argv[]){
   //preparazione per la lettura del tree_raw
   float pos_x , pos_y , pos_z ;
   float pos_xi, pos_yi, pos_zi;
-  float ke_i;
+  float ke_i, ke_f;
 
   std::vector<int> volts      = GetVoltages (folder,subfolder);
   std::vector<int> cnt_radius = GetCNTs     (folder,subfolder);
@@ -61,45 +62,27 @@ int main(int argc, char* argv[]){
   TCanvas* c1 = new TCanvas("c1","",3100,1000);
   c1->cd();
 
-  int z_max      = 4000; //um
-  int y_max      = 4000; //um
+  int z_max      = 8000; //um
+  int y_max      = 8000; //um
   int unit       = 5; //1step in 5 um
   int tes_side   = 60; // um
-  int pixel_side = 2;
+  int pixel_side = 8;
   int x_tes      = 110*unit; 
-  int nbins      = z_max/pixel_side; 
+  int nbins      = 2*z_max/pixel_side; 
     
-  TPad *pad1 = new TPad("pad1","left pad",0,0,0.32,1);
-  pad1->Draw();
-  TPad *pad2 = new TPad("pad2","center pad",0.33,0,0.65,1);
-  pad2->Draw();
-  TPad *pad3 = new TPad("pad3","right pad",0.66,0,1,1);
-  pad3->Draw();
-  pad1->SetLeftMargin (0.15);
-  pad1->SetRightMargin (0.12);
-  pad1->SetBottomMargin (0.12);
-  pad2->SetRightMargin(0.12);
-  pad2->SetLeftMargin(0.12);
-  pad3->SetRightMargin(0.15);
-  pad3->SetLeftMargin(0.12);
  
-  for(int d=0; d<distance.size(); d++){
-    for (int b=0; b<substrate.size(); b++){
-      for(int r=0; r<cnt_radius.size(); r++){
-	for(int v=0; v<volts.size(); v++){
+
+  for(int s=0; s<substrate.size(); s++){
+    for(int d=0; d<distance.size(); d++){
+      for(int v=0; v<volts.size(); v++){
+	for(int r=0; r<cnt_radius.size(); r++){
 	  for(int dg=0; dg<dist_gen.size(); dg++){
 	      
-	    int hits=0;
-	    int discesa=0;
-	    int range      = GetDepartureRange(cnt_radius[r]);
-	    int range_zoom = GetArrivalRange(cnt_radius[r]);
+	   
 	      
 	    //rimb_d600um_cnt15um_sub5000um_V30_dist500um
-	    std::string name = std::string(prefix)+ "_d"
-	      + std::to_string(distance[d]) + "um_cnt"
-	      + std::to_string(cnt_radius[r]) + "um_sub"
-	      + std::to_string(substrate[b]) + "um_V"
-	      + std::to_string(volts[v]) + "_dist"
+	    std::string name = std::string(prefix)+ "_cnt"
+	      + std::to_string(cnt_radius[r]) + "um_dist"
 	      + std::to_string(dist_gen[dg]) + "um";
 	      
 	    //apertura file con il tree
@@ -116,15 +99,45 @@ int main(int argc, char* argv[]){
 	      tree->SetBranchAddress("y_i", &pos_yi);
 	      tree->SetBranchAddress("z_i", &pos_zi);
 	      tree->SetBranchAddress("ke_i", &ke_i);
-		
-	      //mappa arrivi
-	      TH2F *map      = new TH2F("map","",nbins,-y_max,y_max,nbins,-z_max,z_max);
-	      TH2F *map_zoom = new TH2F("map_zoom","",nbins,-y_max,y_max,nbins,-z_max,z_max);
-	      TH1F* energy = new TH1F("energy","",180,-0.5,5);	
-	      //mappa partenze canestri
-	      TH2F *map2      = new TH2F("map2","",nbins,-y_max,y_max,nbins,-z_max,z_max);
-	      TH2F *map2_zoom = new TH2F("map2_zoom","",nbins,-y_max,y_max,nbins,-z_max,z_max);
-		
+	      tree->SetBranchAddress("ke_f", &ke_f);
+	      
+	      
+	      // Bordi dei bin per X e Y
+	      double* zBins = new double[nbins+1];
+	      double* yBins = new double[nbins+1];
+	      for (int i = 0; i <= nbins; ++i) {
+		zBins[i] = -z_max + i * 2*z_max / nbins;
+		zBins[i] -= pixel_side/2;
+		yBins[i] = -y_max + i * 2*y_max / nbins;
+		yBins[i] -= pixel_side/2;
+	      }
+
+	      int hits=0;
+	      int discesa=0;
+	      int range      = GetArrivalRange(cnt_radius[r]);
+	      int range_zoom = GetArrivalRangeZoom(cnt_radius[r]);
+	      int range2     = GetDepartureRange(cnt_radius[r]);
+	      
+	      //mappa arrivi e partenze
+	      TH2F* map = new TH2F("map","",nbins,yBins,nbins,zBins);
+	      TH2F* map_zoom = new TH2F("map_zoom","",nbins,yBins,nbins,zBins);
+	      TH2F* map2 = new TH2F("map2","",nbins,yBins,nbins,zBins);
+	      TH1F* energy = new TH1F("energy","",150,-0.5,5);	
+
+	      TPad* pad1 = new TPad("pad1","left pad",0,0,0.32,1);
+	      pad1->Draw();
+	      TPad* pad2 = new TPad("pad2","center pad",0.33,0,0.65,1);
+	      pad2->Draw();
+	      TPad* pad3 = new TPad("pad3","right pad",0.66,0,1,1);
+	      pad3->Draw();
+	      pad1->SetLeftMargin (0.15);
+	      pad1->SetRightMargin (0.12);
+	      pad1->SetBottomMargin (0.12);
+	      pad2->SetRightMargin(0.12);
+	      pad2->SetLeftMargin(0.12);
+	      pad3->SetRightMargin(0.15);
+	      pad3->SetLeftMargin(0.12);
+	      
 	      for(int iEntry=0; iEntry<nentries; iEntry++){
 		tree->GetEntry(iEntry);
 		//quantità in micron
@@ -139,20 +152,18 @@ int main(int argc, char* argv[]){
 		  
 		//canestro
 		if(pos_x == x_tes &&
-		 pos_y<tes_side*0.5 && pos_y>-tes_side*0.5 &&
-		 pos_z<tes_side*0.5 && pos_z>-tes_side*0.5 ){
-		 hits++;
-        
-		 energy->Fill(ke_i);
+		   pos_y<tes_side*0.5 && pos_y>-tes_side*0.5 &&
+		   pos_z<tes_side*0.5 && pos_z>-tes_side*0.5 ){
+		  hits++;
+		  //energy->Fill(ke_i);
 		  map2->Fill(pos_yi,pos_zi);
-		  map2_zoom->Fill(pos_yi,pos_zi);
-		  } //canestro
+		  
+		} //canestro
 		  
 		  
 	      }//for sulle entries
-	      //disegno totale a sx
-
-	      //energy->Fill(-1);
+		std::cout << discesa << " " << hits << std::endl;	  
+	
 	      TExec *ex1 = new TExec("ex1","gStyle->SetPalette(kBird);");
 	      TExec *ex2 = new TExec("ex2","gStyle->SetPalette(kBird);");
 	      TExec *ex3 = new TExec("ex3","gStyle->SetPalette(kCandy);");
@@ -161,74 +172,65 @@ int main(int argc, char* argv[]){
 	      pad1->cd();
 	      map->Draw("axis");
 	      ex1->Draw();
-	      map->SetTitle(Form("% d arrivals - cnt: %d um, d: %d um, sub=%d, V=%d V, r=%d um",discesa, cnt_radius[r], distance[d], substrate[b], volts[v], dist_gen[dg]));
-	      DrawPad(pad1,map,4000,tes_side, cnt_radius[r]);
+	      map->SetTitle(Form("%.2f%% arrivals - cnt: %d um, d: %d um, sub=%d, V=%d V, r=%d um",discesa*100./nentries, cnt_radius[r], distance[d], substrate[s], volts[v], dist_gen[dg]));
+	      DrawPad(map,range,tes_side, cnt_radius[r]);
 		
 	      //ex2->Draw();
 	      //disegno zoomato al centro
 	      pad2->cd();
 	      map_zoom->Draw("axis");
 	      ex2->Draw();
-	      map_zoom->SetTitle(Form("Hits: %d/%lld",hits,nentries));
-	      DrawPad(pad2,map_zoom,range_zoom, tes_side, cnt_radius[r]);
+	      map_zoom->SetTitle(Form("Hits: %.3f%%",hits*100.0/nentries));
+	      DrawPad(map_zoom,range_zoom, tes_side, cnt_radius[r]);
 		
 	      //disegno totale a sx
 	      pad3->cd();
 	      map2->Draw("axis");
 	      ex3->Draw();
-	      map2->SetTitle(Form("Hits start - cnt: %d um, d: %d um, sub=%d, V=%d V, r=%d um",cnt_radius[r], distance[d], substrate[b], volts[v], dist_gen[dg]));
-	      DrawPad(pad3,map2,range,tes_side,cnt_radius[r]);
+	      map2->SetTitle(Form("Hits start - cnt: %d um, d: %d um, sub=%d, V=%d V, r=%d um",cnt_radius[r], distance[d], substrate[s], volts[v], dist_gen[dg]));
+	      DrawPad(map2,range2,tes_side,cnt_radius[r]);
 		
 		
-	      c1->SaveAs(Form("plots/%s/%s/maps/%s_map_cfr.png",folder,subfolder,name.c_str()));
+	      c1->SaveAs(Form("plots/%s/%s/maps/%s_map.png",folder,subfolder,name.c_str()));
 	      c1->Clear();
-
 	      
-	      TF1 f("cb",Cruijff,0,1,5);
+	      /*
+	      TF1 fitfunction("fitfunction",Cruijff,0,1,5);
 	      double par[5];
-	      f.SetParameters(par);
-	      f.SetLineColor(kRed-4);
-	      f.SetLineWidth(4);
-	      
-	      f.SetParameter(0, 0.2);
-	      f.SetParameter(1, 0.01);
-	      f.SetParameter(2, 0.1);
-	      f.SetParameter(3, 3);
-	      f.SetParameter(4, 10);
-	      f.SetParLimits(0,0.01,0.3);
-	      f.SetParLimits(1,0.001,0.1);
-	      f.SetParLimits(2,0.001,0.1);
-	      f.SetParLimits(4, 10,10000);
-	      f.SetParLimits(3, 0.1,10);
-	      f.SetNpx(1000);
-	      
-	      energy->Fit("cb","RmX","",-1,1);
-	      //legend->AddEntry(fitFcn,"best fit","l");
-    
-   
+	      fitfunction.SetParameters(par);
+	      fitfunction.SetLineColor(kRed-4);
+	      fitfunction.SetLineWidth(4);
+	      fitfunction.SetNpx(1000);
+	
+	      fitfunction.SetParameter(0, 0.2);
+	      fitfunction.SetParameter(1, 0.01);
+	      fitfunction.SetParameter(2, 0.1);
+	      fitfunction.SetParameter(3, 3);
+	      fitfunction.SetParameter(4, 10);
 
+	      fitfunction.SetParLimits(0,0.2,0.3);
+	      fitfunction.SetParLimits(1,0.02,0.1);
+	      fitfunction.SetParLimits(2,0.001,0.1);
+	      fitfunction.SetParLimits(4, 10,10000);
+	      fitfunction.SetParLimits(3, 0.1,10);
 
+	      //energy->GetXaxis()->SetRange(,5);
+	      energy->Fit("fitfunction","RmX","",-0.5,0.7);
 	      energy->Draw("same");
 	      c1->SaveAs(Form("plots/%s/%s/maps/%s_energy.png",folder,subfolder,name.c_str()));
-		
-	      delete(map);
-	      delete(map_zoom);
-	      delete(map2);
-	      delete(map2_zoom);
-		
+	      */
+	      
 	    }//else sul file
 	  }//for dist_gen
-	}//for volts
-	
-      }//for raggio
-    }//for substrate
-  }//for distanza
+	}//for raggio cnt
+      }//for volts
+    }//for distanza
+  }//for substrato
   
-  delete(c1);
   return(0);
 }
 
-void DrawPad(TPad* pad1,TH2F* map,int range, int tes_side, int cnt_radius){
+void DrawPad(TH2F* map,int range, int tes_side, int cnt_radius){
   //area geometrica del TES
   TLine* line1 = new TLine(-tes_side*0.5, -tes_side*0.5, -tes_side*0.5,  tes_side*0.5);
   TLine* line2 = new TLine(-tes_side*0.5,  tes_side*0.5,  tes_side*0.5,  tes_side*0.5);
@@ -263,18 +265,29 @@ void DrawPad(TPad* pad1,TH2F* map,int range, int tes_side, int cnt_radius){
 
 int GetDepartureRange(int cnt_radius){
   int range;
-  if(cnt_radius==15 ){range = 200;}
-  if(cnt_radius==40 ){range = 100;}
-  if(cnt_radius==100){range = 175;}
-  if(cnt_radius==200){range = 250;}
-  if(cnt_radius==300){range = 350;}
-  if(cnt_radius==500){range = 500;}
+  if(cnt_radius==15 ){range = 800;}
+  if(cnt_radius==40 ){range = 150;}
+  if(cnt_radius==100){range = 200;}
+  if(cnt_radius==200){range = 300;}
+  if(cnt_radius==300){range = 400;}
+  if(cnt_radius==500){range = 600;}
+  return range;
+}
+
+int GetArrivalRangeZoom(int cnt_radius){
+  int range;
+  if(cnt_radius==15 ){range = 150;}
+  if(cnt_radius==40 ){range = 150;}
+  if(cnt_radius==100){range = 200;}
+  if(cnt_radius==200){range = 300;}
+  if(cnt_radius==300){range = 400;}
+  if(cnt_radius==500){range = 600;}
   return range;
 }
 
 int GetArrivalRange(int cnt_radius){
   int range;
-  if(cnt_radius==15 ){range = 100;}
+  if(cnt_radius==15 ){range = 7000;}
   if(cnt_radius==40 ){range = 150;}
   if(cnt_radius==100){range = 225;}
   if(cnt_radius==200){range = 300;}
