ee6edceb54017d0ce186ffebe0a60c806102877d
“benni-corcione” <benedetta.corcione@uniroma1.it>
2025-12-03 12:50:45 +0100

    rifiniture per articolo

--- drawMultipleFits.cpp
+++ drawMultipleFits.cpp
@@ -86,9 +86,16 @@ int main(int argc, char* argv[]) {
   float xmin = 0;
   float xmax = 0;
   int nbins;
-  if(CD_number==188){ xmin = 0.73; xmax = 0.93999; nbins = 200;}
-  if(CD_number==204){ xmin = 0.55;  xmax = 0.67; nbins = 700;}
-  if(CD_number==222){ xmin = 0.09; xmax = 0.13; nbins = 1000;}
+  float scale_factor = 1.0;
+    if(CD_number == 204) scale_factor = 1.61;
+    if(CD_number == 222) scale_factor = 8.63;
+    
+  if(CD_number==188){ xmin = 0.73*scale_factor; xmax = 0.93999*scale_factor; nbins = 200*2;}
+  //if(CD_number==204){ xmin = 0.55*scale_factor;  xmax = 0.67*scale_factor; nbins = 896;}
+  //if(CD_number==222){ xmin = 0.09*scale_factor; xmax = 0.13*scale_factor; nbins = 650;}
+
+  if(CD_number==204){ xmin = 0.81;  xmax = 1.12; nbins = 404;}
+  if(CD_number==222){ xmin = 0.81;  xmax = 1.12; nbins = 404;}
   
 
   TCanvas* c1 = new TCanvas( "c1", "", 600, 600 );
@@ -107,34 +114,52 @@ int main(int argc, char* argv[]) {
 
     //creo un histo per ogni tree
     std::string histname(Form("%d", volts[i]));
-    TH1F* h1_amp = new TH1F( histname.c_str(), "", nbins, 0., 1. );
-    tree->Project(histname.c_str(), "amp");
+    //TH1F* h1_amp = new TH1F( histname.c_str(), "", nbins, 0., 1. );
+    //tree->Project(histname.c_str(), "amp");
 
+    //chatgpt
+    TH1F* h1_amp = new TH1F(histname.c_str(), "", nbins, 0, 2.);
+
+    
+    float amp_val;
+    tree->SetBranchAddress("amp", &amp_val);
+
+    for(Long64_t k = 0; k < tree->GetEntries(); k++){
+      tree->GetEntry(k);
+      
+      float new_amp = scale_factor * amp_val;   // riscalamento richiesto
+      h1_amp->Fill(new_amp);
+    }
+    //fine chatgpt
+    
     Fit f_var = SetFitValues(CD_number,"amp",h1_amp,volts[i],(char*)measure);
     
     //assegnazione parametri di fit
-    float mu_min     = f_var.get_mu_min();
-    float mu_max     = f_var.get_mu_max();
-    float mu_        = f_var.get_mu();
-    float sigmaL_min = f_var.get_sigmaL_min();
-    float sigmaL_max = f_var.get_sigmaL_max();
-    float sigmaL_    = f_var.get_sigmaL();
-    float sigmaR_min = f_var.get_sigmaR_min();
-    float sigmaR_max = f_var.get_sigmaR_max();
-    float sigmaR_    = f_var.get_sigmaR();
-    float alfa_min   = f_var.get_alfa_min();
-    float alfa_max   = f_var.get_alfa_max();
-    float alfa_      = f_var.get_alfa();
+    float mu_min     = f_var.get_mu_min()*scale_factor;
+    float mu_max     = f_var.get_mu_max()*scale_factor;
+    float mu_        = f_var.get_mu()*scale_factor;
+    float sigmaL_min = f_var.get_sigmaL_min()*scale_factor;
+    float sigmaL_max = f_var.get_sigmaL_max()*scale_factor;
+    float sigmaL_    = f_var.get_sigmaL()*scale_factor;
+    float sigmaR_min = f_var.get_sigmaR_min()*scale_factor;
+    float sigmaR_max = f_var.get_sigmaR_max()*scale_factor;
+    float sigmaR_    = f_var.get_sigmaR()*scale_factor;
+    float alfa_min   = f_var.get_alfa_min()*scale_factor;
+    float alfa_max   = f_var.get_alfa_max()*scale_factor;
+    float alfa_      = f_var.get_alfa()*scale_factor;
     float A_min      = f_var.get_A_min();
     float A_max      = f_var.get_A_max();
     float A_         = f_var.get_A();
-    float fit_min    = f_var.get_fit_min();
-    float fit_max    = f_var.get_fit_max();
+    float fit_min    = f_var.get_fit_min()*scale_factor;
+    float fit_max    = f_var.get_fit_max()*scale_factor;
    
    TF1 *cruijff  = new TF1(Form("cru_%d", volts[i]), Cruijff, fit_min, fit_max, 5);
 
     //parametri del fit
     cruijff->SetParNames("#mu","#sigma_{L}", "#sigma_{R}", "#alpha", "A");
+    if(CD_number==222){mu_ = 0.985;}
+    if(CD_number==222){sigmaR_max = 0.03;}
+    if(CD_number==204){sigmaL_min = 0.0097;}
     cruijff->SetParLimits(0,    mu_min,    mu_max);
     cruijff->SetParLimits(1,sigmaL_min,sigmaL_max); 
     cruijff->SetParLimits(2,sigmaR_min,sigmaR_max);
@@ -146,6 +171,7 @@ int main(int argc, char* argv[]) {
     cruijff->SetNpx(1000);
     cruijff->SetParameters(mu_, sigmaL_, sigmaR_, alfa_, A_); //parametri iniziali
 
+   
     //quindi di fatto è una gaussiana doppiamente definitita e basta
     cruijff->FixParameter( 3, 0.  ); // alpha
 
@@ -155,17 +181,17 @@ int main(int argc, char* argv[]) {
     h1_amp->SetLineWidth(4); //4 single
     h1_amp->Fit(cruijff, "RX");
     h1_amp->SetXTitle("arbitrary units");
-    h1_amp->SetYTitle(Form("Counts / (%.3f V)", (double)(h1_amp->GetXaxis()->GetBinWidth(13))) );
+    h1_amp->SetYTitle(Form("Counts/%f", (double)(h1_amp->GetXaxis()->GetBinWidth(13))) );
     h1_amp->GetXaxis()->SetTitleSize(0.04);
-    h1_amp->GetYaxis()->SetTitleSize(0.07);
-    h1_amp->GetXaxis()->SetLabelSize(0.065);
-    h1_amp->GetYaxis()->SetLabelSize(0.065);
+    h1_amp->GetYaxis()->SetTitleSize(0.072);
+    h1_amp->GetXaxis()->SetLabelSize(0.068);
+    h1_amp->GetYaxis()->SetLabelSize(0.068);
     h1_amp->GetYaxis()->SetTitleOffset(+1.2);
     h1_amp->GetXaxis()->SetTitleOffset(0.6);
     
     c1->Clear();
     TLatex* latex = new TLatex();
-    latex->SetTextSize(0.065);
+    latex->SetTextSize(0.080);
     latex->SetTextColor(colors[i]);
     
     h1_amp->Draw();
@@ -179,29 +205,35 @@ int main(int argc, char* argv[]) {
 
   int ymax;
   if(CD_number==188){ymax = 280;}
-  if(CD_number==204){ymax = 280;}
-  if(CD_number==222){ymax = 210;}
+  if(CD_number==204){ymax = 560;}
+  if(CD_number==222){ymax = 120;}
   TH2D* h2_axes = new TH2D( "axes", "", 10, xmin, xmax, 10, 0., ymax );
   h2_axes->SetXTitle( "arbitrary units" );
-  h2_axes->GetXaxis()->SetLabelSize(0);  
-  h2_axes->GetXaxis()->SetTickLength(0);  
-  h2_axes->GetXaxis()->SetTitleSize(0.04);
-  h2_axes->GetXaxis()->SetTitleOffset(0.6);
-
+  //h2_axes->GetXaxis()->SetLabelSize(0);  
+  //h2_axes->GetXaxis()->SetTickLength(0);  
+  h2_axes->GetXaxis()->SetTitleSize(0.05);
+  h2_axes->GetYaxis()->SetTitleSize(0.05);
+  h2_axes->GetYaxis()->SetLabelSize(0.05);
+  h2_axes->GetXaxis()->SetTitleSize(0.048);
+  h2_axes->GetXaxis()->SetLabelSize(0.048);
+  h2_axes->GetXaxis()->SetTitleOffset(1.0);
+  if(CD_number==204){h2_axes->GetYaxis()->SetNdivisions(510);}
+  if(CD_number==222){h2_axes->GetYaxis()->SetNdivisions(510);}
+  h2_axes->GetXaxis()->SetNdivisions(510);
   //h2_axes->GetXaxis()->SetNdivisions(505);
   h2_axes->GetYaxis()->SetTitleOffset(1.5);
-  h2_axes->SetYTitle( Form("Counts / (%.3f V)", (double)(histos[0]->GetXaxis()->GetBinWidth(13))) );
-  if(CD_number==204){h2_axes->SetTitle("T-60, CNTs: 1 mm^{2}");}
-  if(CD_number==222){h2_axes->SetTitle("T-100-b, CNTs: 1 mm^{2}");}
+  h2_axes->SetYTitle( Form("Counts", (double)(histos[0]->GetXaxis()->GetBinWidth(13))) );
+  if(CD_number==204){h2_axes->SetTitle("T60, CNTs: 1 mm^{2}");}
+  if(CD_number==222){h2_axes->SetTitle("T100b, CNTs: 1 mm^{2}");}
   h2_axes->Draw();
   
   TLatex* latex = new TLatex();
-  latex->SetTextSize(0.035);
+  latex->SetTextSize(0.05);
   //style->SetOptTitle(1);
   
   for( unsigned i=0; i<histos.size(); ++i ) {
 
-    histos[i]->SetLineWidth(2);
+    histos[i]->SetLineWidth(3);
     histos[i]->SetLineColor(colors[i]);
     //histos[i]->GetXaxis()->SetNdivisions(0);
     TF1* f = histos[i]->GetFunction(Form("cru_%s", histos[i]->GetName()));
@@ -216,22 +248,30 @@ int main(int argc, char* argv[]) {
     }
     if(CD_number==204){
       
-      if(i==0){ latex->DrawLatex( 0.627, 220, Form("#it{V}_{CNT} = %s V", histos[i]->GetName()));}
-      if(i==1){ latex->DrawLatex( 0.582, 250, Form("#it{V}_{CNT} = %s V", histos[i]->GetName()) );}}
+      if(i==0){ latex->DrawLatex( 1.005, 450, Form("#it{V}_{CNT} = %s V", histos[i]->GetName()));}
+      if(i==1){ latex->DrawLatex( 0.875, 500, Form("#it{V}_{CNT} = %s V", histos[i]->GetName()) );}}
     
   
   
   if(CD_number==222){
-    histos[i]->SetTitle("T-100-b, CNTs: 1 mm^{2}");
-    if(i==0){ latex->DrawLatex( 0.102, 180, Form("#it{V}_{CNT} = %s V", histos[i]->GetName()));}
-    if(i==1){ latex->DrawLatex( 0.118, 160, Form("#it{V}_{CNT} = %s V", histos[i]->GetName()) );}}
+    histos[i]->SetTitle("T100b, CNTs: 1 mm^{2}");
+    if(i==0){ latex->DrawLatex( 0.855, 100, Form("#it{V}_{CNT} = %s V", histos[i]->GetName()));}
+    if(i==1){ latex->DrawLatex( 0.115*scale_factor, 110, Form("#it{V}_{CNT} = %s V", histos[i]->GetName()) );}}
   
 
   }
-  gPad->RedrawAxis();
 
-  c1->SaveAs(Form("plots/articolo2/fits_CD%d.pdf",CD_number));
+  /*
+  TLine* line = new TLine(0.9995, 0, 0.9995, 102 );
+  line->SetLineColor(kBlack);
+  line->SetLineStyle( 2 );
+  line->SetLineWidth( 2 );
+  line->Draw("same");
+  */
+  gPad->RedrawAxis();
   
+  //c1->SaveAs(Form("/Users/massimo/Documents/phd/PTOLEMY/Articoli/Firmati/new article/graphs/fits_CD%d.pdf",CD_number));
+  c1->SaveAs(Form("/Users/massimo/Documents/phd/PTOLEMY/CD Data/plots/articolo2/fits_CD%d.pdf",CD_number));
 
   c1->Clear();
 
